99058a356233d73cce9f766cf064f814223055dc
Saiful Alom <saifulalom49590@gmail.com>
2026-01-10 23:57:10 +0600

    Add R2 storage client and signed URL generation
    - Implemented R2 client using AWS SDK for S3 in r2.client.js
    - Created a function to request signed upload URLs in r2.signed-url.js

---
+++ backend/src/infrastructure/storage/r2.client.js
@@ -0,0 +1,11 @@
+// FILE: src/infrastructure/storage/r2.client.js
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const r2Client = new S3Client({
+  region: "auto",
+  endpoint: process.env.R2_END_POINT,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+});
